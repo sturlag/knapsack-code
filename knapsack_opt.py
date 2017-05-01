1cbf5471f0ff8e72d2552ae2d10c678883f41c84
Shalom Abate <shaladi@mit.edu>
2017-05-01 12:19:29 -0400

    Fix opt knapsack algorithm

--- knapsack_opt.py
+++ knapsack_opt.py
@@ -1,5 +1,6 @@
 def opt(E,i=0,c=1,memo={}):
-    print memo
+    if c <= 0:
+        return (set(), 0)
     if (i,c) in memo:
         return memo[(i,c)]
     if i == len(E)-1:
@@ -8,11 +9,15 @@ def opt(E,i=0,c=1,memo={}):
         else:
             return (set(), 0)
     (A,valA) = opt(E,i+1,c,memo) # don't include E[i]
-    (B, valB) = opt(E,i+1, c-E[i].s,memo) # include E[i]
-    if (valA < valB + E[i].s):
-        B.add(E[i])
-        memo[(i,c)] = (B, valB + E[i].s)
-        return (B, valB + E[i].s)
+    (B,valB) = opt(E,i+1, c-E[i].s,memo) # include E[i]
+    B.add(E[i])
+    valB += E[i].s
+    if valB > c: # If it exceeds capacity, then no go.
+        valB = 0
+        B = set()
+    if (valA < valB):
+        memo[(i,c)] = (B, valB)
+        return (B, valB)
     else:
         memo[(i,c)] = (A, valA)
         return (A, valA)
